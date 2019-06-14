f12df8751a8e9f1b8bc05ae4eb866a1cc50d280c
PkR <pkr@pkr.pkr>
2019-06-15 01:38:07 +0900

    Re-exported Egg Hornet

--- DCMods_Main/Boss_EGM1_EggHornet.cpp
+++ DCMods_Main/Boss_EGM1_EggHornet.cpp
@@ -1,8 +1,9 @@
 #include "stdafx.h"
-#include "EggHornet_Water.h"
 
 NJS_TEXNAME textures_egm1land[118];
 NJS_TEXLIST texlist_egm1land = { arrayptrandlength(textures_egm1land) };
+
+//#include "EggHornet.h"
 
 DataArray(PVMEntry, EggHornetObjectTextures, 0x156F6B0, 15);
 DataArray(FogData, EggHornetFog, 0x01556B34, 3);
@@ -15,85 +16,115 @@ static int EggHornet_Rotation = 0;
 static int EggHornet_RotationDirection = 1;
 static bool EggHornet_RotationEnabled = false;
 
-void __cdecl EggHornetWaterFunc()
+NJS_OBJECT* EHOcean = nullptr;
+NJS_OBJECT* EHWaterObjects[] = { nullptr, nullptr, nullptr, nullptr, nullptr };
+
+void AddEHWaterObject(NJS_OBJECT *object)
 {
-	if (B_EGM1_Info && !DroppedFrames)
+	for (int q = 0; q < LengthOfArray(EHWaterObjects); ++q)
 	{
-		if (EnableEggHornet == true && !SADXWater_MysticRuins && CurrentLevel == 20 && GameState != 16)
+		if (EHWaterObjects[q] == object) return;
+		else if (EHWaterObjects[q] == nullptr)
 		{
-			if (FramerateSetting < 2 && FrameCounter % 4 == 0 || FramerateSetting == 2 && FrameCounter % 2 == 0 || FramerateSetting > 2)
-			{
-				EggHornetOceanAnimationSA1++;
-			}
-			if (EggHornetOceanAnimationSA1 > 9) EggHornetOceanAnimationSA1 = 0;
-			matlist_00057F04[0].attr_texId = EggHornetOceanAnimationSA1;
+			EHWaterObjects[q] = object;
+			return;
 		}
+	}
+}
+
+void __cdecl EHWater_Display(void(__cdecl *function)(void *), void *data, float depth, QueuedModelFlagsB queueflags)
+{
+	if (SADXWater_MysticRuins)
+	{
+		DrawModelCallback_Queue((void(__cdecl *)(void *))MysticRuins_OceanDraw, OceanDataArray, -7952.0f, QueuedModelFlagsB_EnableZWrite);
+	}
+	else
+	{
+		ClampGlobalColorThing_Thing();
 		DisableFog();
-		njSetTexture(&texlist_egm1land);
-		njPushMatrix(0);
-		njTranslate(0, 0, 0, 0);
-		ProcessModelNode_A_Wrapper((NJS_OBJECT*)B_EGM1_Info->getdata("object_00048F9C"), QueuedModelFlagsB_3, 1.0f);
-		njPopMatrix(1u);
-		njPushMatrix(0);
-		njTranslate(0, 0, 0, 0);
-		ProcessModelNode_A_Wrapper((NJS_OBJECT*)B_EGM1_Info->getdata("object_00049370"), QueuedModelFlagsB_3, 1.0f);
-		njPopMatrix(1u);
-		njPushMatrix(0);
-		njTranslate(0, 0, 0, 0);
-		ProcessModelNode_A_Wrapper((NJS_OBJECT*)B_EGM1_Info->getdata("object_0004EB6C"), QueuedModelFlagsB_3, 1.0f);
-		njPopMatrix(1u);
-		njPushMatrix(0);
-		njTranslate(0, 0, 0, 0);
-		ProcessModelNode_A_Wrapper((NJS_OBJECT*)B_EGM1_Info->getdata("object_0004EE14"), QueuedModelFlagsB_3, 1.0f);
-		njPopMatrix(1u);
 		njSetTexture(&BEACH_SEA_TEXLIST);
 		njPushMatrix(0);
-		njTranslate(0, Camera_Data1->Position.x, 0, Camera_Data1->Position.z);
-		njScale(0, 1.0f, 1.0f, 1.0f);
-		DrawQueueDepthBias = -47000;
-		ProcessModelNode(&object_00057FD4, QueuedModelFlagsB_EnableZWrite, 1.0f);
+		njTranslate(0, 0, 0, 0);
+		DrawQueueDepthBias = -47952.0f;
+		if (EHOcean) ProcessModelNode_AB_Wrapper(EHOcean, 1.0f);
+		DrawQueueDepthBias = 0.0f;
 		njPopMatrix(1u);
-		DrawQueueDepthBias = 0;
+	}
+	for (unsigned int i = 0; i < LengthOfArray(EHWaterObjects); i++)
+	{
+		if (EHWaterObjects[i])
+		{
+			njSetTexture(&texlist_egm1land);
+			njPushMatrix(0);
+			njTranslate(0, 0, 0, 0);
+			ProcessModelNode_A_Wrapper(EHWaterObjects[i], QueuedModelFlagsB_3, 1.0f);
+			njPopMatrix(1u);
+		}
 	}
 }
 
 void UnloadLevelFiles_B_EGM1()
 {
+	LandTable *B_EGM1 = B_EGM1_Info->getlandtable();
+	NJS_MATERIAL *material;
 	//Also unregister white diffuse in the level
+	for (unsigned int j = 0; j < B_EGM1->COLCount; j++)
+	{
+		for (int k = 0; k < B_EGM1->Col[j].Model->basicdxmodel->nbMat; ++k)
+		{
+			material = (NJS_MATERIAL*)&B_EGM1->Col[j].Model->basicdxmodel->mats[k];
+			if (material->attrflags & NJD_CUSTOMFLAG_WHITE)
+			{
+				RemoveWhiteDiffuseMaterial(material);
+			}
+		}
+	}
 	delete B_EGM1_Info;
 	B_EGM1_Info = nullptr;
 }
 
 void LoadLevelFiles_B_EGM1()
 {
+	Uint32 materialflags;
+	NJS_MATERIAL *material;
 	CheckAndUnloadLevelFiles();
 	B_EGM1_Info = new LandTableInfo(HelperFunctionsGlobal.GetReplaceablePath("SYSTEM\\data\\B_EGM1\\0.sa1lvl"));
-	LandTable *B_EGM1 = B_EGM1_Info->getlandtable();
+	LandTable *B_EGM1 = B_EGM1_Info->getlandtable(); //&landtable_00000128;
 	RemoveMaterialColors_Landtable(B_EGM1);
 	B_EGM1->TexList = &texlist_egm1land;
 	LandTableArray[40] = B_EGM1;
-	//SADX style water
-	if (SADXWater_MysticRuins)
+	for (unsigned int j = 0; j < B_EGM1->COLCount; j++)
 	{
-		EggHornetObjectTextures[0].TexList = &texlist_sadxwtr_waves;
-		EggHornetObjectTextures[0].Name = "SADXWTR_WAVES";
-		WriteJump(EggHornet_OceanDraw, MysticRuins_OceanDraw_SADXStyle); //Egg Hornet ocean
-		B_EGM1->Col[B_EGM1->COLCount - 1].Flags = 0x81000000;
-		B_EGM1->Col[B_EGM1->COLCount - 2].Flags = 0x81000000;
-		B_EGM1->Col[B_EGM1->COLCount - 3].Flags = 0x81000000;
-		B_EGM1->Col[B_EGM1->COLCount - 4].Flags = 0x81000000;
+		//Check materials
+		for (int k = 0; k < B_EGM1->Col[j].Model->basicdxmodel->nbMat; ++k)
+		{
+			material = (NJS_MATERIAL*)&B_EGM1->Col[j].Model->basicdxmodel->mats[k];
+			//Texanim 1
+			if (material->attr_texId == 55)
+			{
+				material->attr_texId = 0;
+				AddTextureAnimation(0, material, false, 5, 0, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+			}
+			//White diffuse
+			if (material->attrflags & NJD_CUSTOMFLAG_WHITE)
+			{
+				AddWhiteDiffuseMaterial(material);
+			}
+		}
+		//Check COL flags
+		if (B_EGM1->Col[j].Flags & 0x8000000)
+		{
+			//if (B_EGM1->Col[j].Flags & ColFlags_Visible) B_EGM1->Col[j].Flags &= ~ColFlags_Visible;
+			if (B_EGM1->Col[j].Flags & 0x2)
+			{
+				EHOcean = B_EGM1->Col[j].Model;
+			}
+			else
+			{
+				AddEHWaterObject(B_EGM1->Col[j].Model);
+			}
+		}
 	}
-	else
-	{
-		EggHornetObjectTextures[0].TexList = &BEACH_SEA_TEXLIST;
-		EggHornetObjectTextures[0].Name = "BEACH_SEA";
-		WriteJump(EggHornet_OceanDraw, EggHornetWaterFunc); //Egg Hornet water
-		B_EGM1->Col[B_EGM1->COLCount - 1].Flags = 0x00000000;
-		B_EGM1->Col[B_EGM1->COLCount - 2].Flags = 0x00000000;
-		B_EGM1->Col[B_EGM1->COLCount - 3].Flags = 0x00000000;
-		B_EGM1->Col[B_EGM1->COLCount - 4].Flags = 0x00000000;
-	}
-	//WhiteDiffuse_EGM1External[0] = &((NJS_MATERIAL*)B_EGM1_Info->getdata("matlist_0005B834"))[0];
 }
 
 void RotateEggHornet(ObjectMaster *a1)
@@ -130,8 +161,22 @@ void EggHornet_Init()
 	ReplacePVM("EGM1SORA");
 	ReplacePVM("EGM1TSUCHI");
 	ResizeTextureList((NJS_TEXLIST*)0x1557064, 118); //Egg Hornet level texlist
+	RemoveVertexColors_Object((NJS_OBJECT*)0x1561A70); //Egg Hornet main model
 	RemoveVertexColors_Object((NJS_OBJECT*)0x1561A70); //Egg Hornet eggmobile part
 	RemoveVertexColors_Object((NJS_OBJECT*)0x3306270); //Egg Hornet cutscene model
+	//Water
+	WriteCall((void*)0x57192A, EHWater_Display); //Egg Hornet water
+	//SADX style water
+	if (SADXWater_MysticRuins)
+	{
+		EggHornetObjectTextures[0].TexList = &texlist_sadxwtr_waves;
+		EggHornetObjectTextures[0].Name = "SADXWTR_WAVES";
+	}
+	else
+	{
+		EggHornetObjectTextures[0].TexList = &BEACH_SEA_TEXLIST;
+		EggHornetObjectTextures[0].Name = "BEACH_SEA";
+	}
 	//Egg Hornet model stuff
 	((NJS_OBJECT*)0x0155AA54)->basicdxmodel->mats[0].attrflags |= NJD_FLAG_IGNORE_SPECULAR;
 	((NJS_OBJECT*)0x0155AA54)->basicdxmodel->mats[1].attrflags |= NJD_FLAG_IGNORE_SPECULAR;
@@ -161,11 +206,7 @@ void EggHornet_Init()
 	((NJS_OBJECT*)0x01561A70)->basicdxmodel->mats[18].attrflags |= NJD_FLAG_IGNORE_LIGHT; //front light
 	((NJS_OBJECT*)0x01561A70)->basicdxmodel->mats[8].attrflags |= NJD_FLAG_IGNORE_LIGHT; //computer
 	((NJS_MATERIAL*)0x015570AC)->attrflags &= ~NJD_FLAG_IGNORE_SPECULAR;
-	for (int m = 0; m < 20; m++)
-	{
-		((NJS_OBJECT*)0x01561A70)->basicdxmodel->mats[m].attrflags &= ~NJD_FLAG_IGNORE_SPECULAR;
-	}
-	//*(NJS_OBJECT*)0x1561A70 = object_000104E8; //Egg Hornet model
+	ForceObjectSpecular_Object((NJS_OBJECT*)0x1561A70);
 	for (unsigned int i = 0; i < 3; i++)
 	{
 		DrawDist_EggHornet[i].Maximum = -12500.0;
