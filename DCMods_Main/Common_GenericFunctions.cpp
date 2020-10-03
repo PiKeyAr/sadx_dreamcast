9cee0a862fee5e7a7c8b597d683da93ec80be25b
PkR <pkr@pkr.pkr>
2020-10-04 00:31:49 +0900

    Shieeet

--- DCMods_Main/Common_GenericFunctions.cpp
+++ DCMods_Main/Common_GenericFunctions.cpp
@@ -15,21 +15,36 @@ void RestoreDebugFontSettings()
 
 void njDrawQuadTexture_Italic(NJS_QUAD_TEXTURE* points, float scale)
 {
+	double widthmaybe; // st7
+	Float base_x; // ecx
+	Float v4; // edx
+	double v5; // st7
+	Float v6; // ecx
+	double v7; // st7
+	double v8; // st7
 	NJS_QUAD_TEXTURE_EX _points; // [esp+0h] [ebp-40h]
 
-	_points.x = points->x1 + DebugFontItalic * 4.0f; //offset for accuracy
+	widthmaybe = points->x2 - points->x1;
+	base_x = points->x1;
 	_points.y = points->y1;
+	v4 = points->u1;
+	_points.vx1 = widthmaybe + DebugFontItalic * 4.0f; //width
+	_points.x = base_x + DebugFontItalic * 4.0f; //offset for accuracy
+	v5 = points->y2 - points->y1;
+	_points.u = v4;
 	_points.z = scale;
-	_points.u = points->u1;
-	_points.v = points->v1;
-	_points.vx1 = points->x2 - points->x1 + DebugFontItalic * 4.0f; //width
-	_points.vx2 = DebugFontItalic * -10.0f; //italics
+	v6 = points->v1;
+	_points.vy2 = v5;
+	v7 = points->u2 - points->u1;
 	_points.vy1 = 0.0f;
-	_points.vy2 = points->y2 - points->y1;
-	_points.vu1 = points->u2 - points->u1;
+	_points.vx2 = 0.0f;
+	_points.v = v6;
+	_points.vu1 = v7;
+	_points.vv1 = 0.0f;
+	v8 = points->v2;
 	_points.vu2 = 0.0f;
-	_points.vv2 = points->v2 - points->v1;
-
+	_points.vv2 = v8 - points->v1;
+	_points.vx2 -= DebugFontItalic * 10.0f;
 	Direct3D_DrawQuad(&_points);
 }
 
